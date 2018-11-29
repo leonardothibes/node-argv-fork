f9d095c25938a5361093cedc289c38c10cdb057a
Leonardo Thibes <leonardothibes@gmail.com>
2018-11-29 15:12:12 -0200

    strict syntax

--- index.js
+++ index.js
@@ -2,62 +2,62 @@
 
 /*!
  * node-argv
- * Copyright(c) 2014 Deploy.Today <support+node@deploy.today>
+ * Copyright(c) 2018 Leonardo Thibes <leonardothibes@gmail.com>
  * @license MIT
  */
+
+/**
+ * Module constants
+ */
+const rSplit = /\s*(?:"|')(.+?)(?:"|'|$)|\s+/,
+    din = '_',
+    don = '--';
 
 /*
  * Module dependencies
  */
-
-var parseArray = require('minimist');
-
-/**
- * Module exports
- */
-
-module.exports = parse;
-
-/**
- * Module variables
- */
-
-var rSplit = /\s*(?:"|')(.+?)(?:"|'|$)|\s+/
-  , din = '_'
-  , don = '--';
+const parseArray = require('minimist');
 
 /**
  * Parse arguments.
  *
- * @param {argv} String/Array to parse
- * @param {opts} Object of properties
- * @param {target} Object
+ * @param {String|Array}  argv
+ * @param {Object}        opts
+ * @param {Object}        target
+ * 
  * @return {target|Object}
  * @api public
  */
+module.exports = function (argv, opts, target) 
+{
+    if ('string' === typeof argv) {
+        argv = argv.split(rSplit).filter(ignore);
+    }
 
-function parse (argv, opts, target) {
-  if ('string' === typeof argv) argv = argv.split(rSplit).filter(ignore);
-  if (!opts) opts = {};
-  opts[don] = true;
-  var parsed = parseArray(argv, opts);
-  opts[don] = false;
-  var through = parsed[don] ? parseArray(parsed[don], opts) : null;
-  if (!target) target = {};
-  target.options = parsed;
-  target.commands = parsed[din];
-  target.input = argv;
-  if (through) {
-    target.through = {
-      options: through,
-      commands: through[din]
-    };
-    delete through[din];
-  }
-  delete parsed[din];
-  delete parsed[don];
-  return target;
-  function ignore (s) {
+    opts        = opts || {};
+    opts[don]   = true;
+    var parsed  = parseArray(argv, opts);
+    opts[don]   = false;
+    var through = parsed[don] ? parseArray(parsed[don], opts) : null;
+
+    target          = target || {};
+    target.options  = parsed;
+    target.commands = parsed[din];
+    target.input    = argv;
+
+    if (through) {
+        target.through = {
+            options: through,
+            commands: through[din]
+        };
+        delete through[din];
+    }
+    delete parsed[din];
+    delete parsed[don];
+
+    return target;
+};
+
+function ignore(s) {
     return s && '' !== s;
-  }
 }
